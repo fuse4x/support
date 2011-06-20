d7df38ed21d70229a3b8f1302b60508a96a790e4
Anatol Pomozov <anatol.pomozov@gmail.com>
2011-06-20 12:16:05 -0700

    Remove sshfs from the bundle

--- uninstall.sh
+++ uninstall.sh
@@ -9,8 +9,6 @@ rm /System/Library/LaunchAgents/org.fuse4x.autoupdater.plist
 rm ~/Library/Preferences/org.fuse4x.autoupdater.plist
 rm ~root/Library/Preferences/org.fuse4x.autoupdater.plist
 rm -r /System/Library/Extensions/fuse4x.kext/
-rm /usr/local/bin/sshfs
-rm /usr/local/share/man/man1/sshfs.1
 rm -r /usr/local/include/fuse*
 rm /usr/local/lib/libfuse4x.*
 rm /usr/local/lib/pkgconfig/fuse.pc
