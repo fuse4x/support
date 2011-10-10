23d7b841824d5c97f5e2e4b54465e8cde22e2cb4
Anatol Pomozov <anatol.pomozov@gmail.com>
2011-10-10 16:16:12 -0700

    Introduce --release flag
    When a module is built for final distribution it is called with --release flag.
    When this flag is enabled a module usually do stuff like purge local changes
    and set correct build flags.
    
    Remove hardcoded SDK dependency from XCode files. XCode should use the default one.
    When we build module for distribution we just set target SDK via command line arguments.

--- build.rb
+++ build.rb
@@ -1,24 +1,29 @@
 #!/usr/bin/env ruby
 # Possible flags are:
-#   --debug       this builds distribuition with debug flags enabled
+#   --release     build this module for final distribuition
 #   --root DIR    install the binary into this directory. If this flag is not set - the script
 #                 redeploys kext to local machine and restarts it
-#   --clean       clean before build
 
 CWD = File.dirname(__FILE__)
 BUNDLE_DIR = '/System/Library/Filesystems/fuse4x.fs'
 Dir.chdir(CWD)
 
-debug = ARGV.include?('--debug')
-clean = ARGV.include?('--clean')
+release = ARGV.include?('--release')
 root_dir = ARGV.index('--root') ? ARGV[ARGV.index('--root') + 1] : nil
 
 abort("root directory #{root_dir} does not exist") if ARGV.index('--root') and not File.exists?(root_dir)
 
-system('git clean -xdf') if clean
+system('git clean -xdf') if release
 
-configuration = debug ? 'Debug' : 'Release'
-system("xcodebuild SYMROOT=build SHARED_PRECOMPS_DIR=build -PBXBuildsContinueAfterErrors=0 -parallelizeTargets -configuration #{configuration} -alltargets") or abort("cannot build kext")
+configuration = release ? 'Release' : 'Debug'
+flags = '-configuration ' + configuration
+if release then
+  flags += ' MACOSX_DEPLOYMENT_TARGET=10.5'
+else
+  flags += ' ONLY_ACTIVE_ARCH=YES'
+end
+
+system("xcodebuild SYMROOT=build SHARED_PRECOMPS_DIR=build -PBXBuildsContinueAfterErrors=0 -parallelizeTargets -alltargets #{flags}") or abort("cannot build kext")
 
 install_path = root_dir ? File.join(root_dir, BUNDLE_DIR) : BUNDLE_DIR
 system("sudo mkdir -p #{install_path}")
